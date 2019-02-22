cdc2df1bf3eca9cafc21c61ad54ef3b8ee1076e7
Yan Papkou <y.papkou@godeltech.com>
2019-02-22 13:30:12 +0300

    update returned format type for web api; set the routing depending on the folder name; add home ApiController; update packages; add global.asax, global.asax.cs;

---
+++ MVC_WebAPI_Example.WebAPI/Api/v1/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+
+namespace MVC_WebAPI_Example.WebAPI.Api.v1
+{
+    public class HomeController : ApiController
+    {
+        // GET: api//v1/Home
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api//v1/Home/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/v1/Home
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/v1/Home/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/v1/Home/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ MVC_WebAPI_Example.WebAPI/App_Start/WebApiConfig.cs
@@ -0,0 +1,26 @@
+﻿using System.Net.Http.Formatting;
+using System.Web.Http;
+
+namespace MVC_WebAPI_Example.WebAPI
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{folder}/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            // returrn Json instead XML;
+            config.Formatters.Clear();
+            config.Formatters.Add(new JsonMediaTypeFormatter());
+        }
+    }
+}

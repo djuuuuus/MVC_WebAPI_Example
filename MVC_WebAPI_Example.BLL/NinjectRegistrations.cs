252ef43968992f516ed31a8ebcdad37ca29118b4
Yan Papkou <y.papkou@godeltech.com>
2019-02-23 22:22:05 +0300

    MVC implement DI;

---
+++ MVC_WebAPI_Example.BLL/NinjectRegistrations.cs
@@ -0,0 +1,14 @@
+﻿using MVC_WebAPI_Example.BLL.Common.Interfaces;
+using MVC_WebAPI_Example.BLL.Services;
+using Ninject.Modules;
+
+namespace MVC_WebAPI_Example.BLL
+{
+    public class NinjectRegistrations : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<ITestService>().To<TestService>();
+        }
+    }
+}
